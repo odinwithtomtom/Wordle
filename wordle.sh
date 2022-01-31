a69f10e89ede236a5a0e44603243462c823909f4
odinwithtomtom <97411616+odinwithtomtom@users.noreply.github.com>
2022-01-31 14:27:36 -0500

    Added function to exclude known incorrect letters

--- wordle.sh
+++ wordle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# This was written to work with Ubuntu. Change the path in the
-# WORDS variable below if your distro stores its dictionary in
+# This was written with Ubuntu in mind. Change the path in the 
+# WORDS variable below if your distro stores its dictionary in 
 # a different location.
 #
 # Use periods to represent unknown characters.
@@ -16,7 +16,9 @@ read -p 'Use periods to represent unknown characters.  ' PATTERN
 LEN=`echo $PATTERN | wc -c`
 LEN=`expr $LEN - 1`
 done
+read -p 'Enter letters to be excluded - no spaces. ' OMIT
+OMIT=`echo $OMIT | awk '$1=$1' FS= OFS=" -e "`
 echo '-----------------------------------------------'
 echo 'The following words match the pattern provided.'
 echo '-----------------------------------------------'
-cat $WORDS | grep -w $PATTERN | grep -v ".....'s"
+cat $WORDS | grep -w $PATTERN | grep -v ".....'s" | grep -v "....'s" | grep -v "...'s" | grep -v -e $OMIT
